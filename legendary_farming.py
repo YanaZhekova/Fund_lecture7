b5df986a11ce9b1d2cd3178fa6da05df35a75b08
YanaZhekova <ianagekowa@gmail.com>
2024-07-07 12:21:53 +0300

    Lecture 7 exercises

--- legendary_farming.py
+++ legendary_farming.py
@@ -2,7 +2,7 @@ info = input().split(" ")
 
 items = dict()
 is_obtained = False
-while not is_obtained:
+while True:
     for i in range(0, len(info), 2):
         quantity = int(info[i])
         item = info[i + 1].lower()
@@ -12,28 +12,46 @@ while not is_obtained:
         else:
             items[item] = quantity
 
-    for i in items.keys():
-        if items[i] >= 250:
-            items[i] -= 250
-            if i == "shards":
-                print("Shadowmourne obtained!")
-            elif i == "motes":
-                print("Dragonwrath obtained!")
-            elif i == "fragments":
-                print("Valanyr obtained!")
+        if item == "shards" and items[item] >= 250:
+            print("Shadowmourne obtained!")
+            items[item] -= 250
             is_obtained = True
+            break
+        elif item == "motes" and items[item] >= 250:
+            print("Dragonwrath obtained!")
+            items[item] -= 250
+            is_obtained = True
+            break
+        elif item == "fragments" and items[item] >= 250:
+            print("Valanyr obtained!")
+            items[item] -= 250
+            is_obtained = True
+            break
 
+    if is_obtained:
+        break
     info = input().split(" ")
 
-for i in items.keys():
-    if i == "shards":
-        print(f"{i}: {items[i]}")
-    elif i == "fragments":
-        print(f"{i}: {items[i]}")
-    elif i == "motes":
-        print(f"{i}: {items[i]}")
+for a in range(3):
+    if a == 0:
+        item = "shards"
+        if item in items.keys():
+            print(f"{item}: {items[item]}")
+        else:
+            print(f"{item}: 0")
+    elif a == 1:
+        item = "fragments"
+        if item in items.keys():
+            print(f"{item}: {items[item]}")
+        else:
+            print(f"{item}: 0")
+    elif a == 2:
+        item = "motes"
+        if item in items.keys():
+            print(f"{item}: {items[item]}")
+        else:
+            print(f"{item}: 0")
 
 for i in items.keys():
     if i != "shards" and i != "fragments" and i != "motes":
         print(f"{i}: {items[i]}")
-
